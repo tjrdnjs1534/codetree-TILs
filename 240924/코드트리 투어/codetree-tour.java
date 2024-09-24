6faa6402380a4e23d519da1d1ecf7648d986ac52
장석원 SeokWon Jang <64577038+tjrdnjs1534@users.noreply.github.com>
2024-09-24 15:18:56 +0900

    Title: 코드트리 투어, Time: 282ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 240924/코드트리 투어/codetree-tour.java
+++ 240924/코드트리 투어/codetree-tour.java
@@ -1,133 +1,178 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    static int Q;
-    static StringTokenizer st;
-    static int start = 0;
-    static int n;
-    static int m;
-    static HashMap<Integer, Product> products = new HashMap<>();
-    static int[][] cost;
-    static int[][] nodes;
-    static StringBuilder sb = new StringBuilder();
-    public static class Product{
-        int revenue;
-        int dest;
-        Product(int r, int d){
-            this.revenue = r;
-            this.dest= d;
-        }
-    }
-
     
+    static int Q, start;
+    static final int MAX = Integer.MAX_VALUE;
+    static int[] dist = new int[2001];
+    static boolean[] checkId = new boolean[30001];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Q = Integer.parseInt(br.readLine());
+    static class LandInfo {
+        int u, w;
 
-        for(int i=0; i<Q; i++){
-            st = new StringTokenizer(br.readLine());
-            int op = Integer.parseInt(st.nextToken());
-            if(op ==100){
-                n = Integer.parseInt(st.nextToken());
-                m = Integer.parseInt(st.nextToken());
-                nodes = new int[n][n];
-                cost = new int[n][n];
-                for(int r=0; r<n; r++){
-                    for(int c=0; c<n; c++){
-                        if(r==c)continue;
-                        cost[r][c] = 100000;
-                    }
-                }
-
-                for(int k = 0; k<m; k++){
-                    int v = Integer.parseInt(st.nextToken());
-                    int u = Integer.parseInt(st.nextToken());
-                    int w = Integer.parseInt(st.nextToken());
-                    if(v==u) continue;
-                    if(nodes[v][u] == 0 ){
-                        nodes[v][u] = w;
-                        nodes[u][v] = w;
-                        cost[v][u] = w;
-                        cost[u][v] = w;
-                    }
-                    else {
-                        nodes[v][u] = Math.min(nodes[v][u],w);
-                        nodes[u][v] = Math.min(nodes[u][v],w);
-                        cost[v][u] = nodes[v][u];
-                        cost[u][v] = nodes[u][v];
-                    }
-                }
-                ploid();
-            }
-            else if(op==200) {
-                int pid = Integer.parseInt(st.nextToken());
-                int r = Integer.parseInt(st.nextToken());
-                int d = Integer.parseInt(st.nextToken());
-                products.put(pid, new Product(r,d));
-            }
-            else if(op==300){
-                int pid = Integer.parseInt(st.nextToken());
-                if(!products.containsKey(pid)) continue;
-                products.remove(pid);
-            }
-            else if(op==400) {
-                int ans = find();
-                // print();
-                // System.out.println();
-                sb.append(ans);
-                sb.append("\n");
-            }
-            else if(op==500){
-                int s =Integer.parseInt(st.nextToken());
-                start = s;
-            }
+        LandInfo(int u, int w) {
+            this.u = u;
+            this.w = w;
         }
-        
-        System.out.print(sb.toString());
     }
-    public static void ploid() {
-        for(int k =0; k<n; k++){
-            for(int i=0; i<n; i++){
-                for(int j=0; j<n; j++){
-                    cost[i][j] = Math.min(cost[i][j], cost[i][k] + cost[k][j]);
+
+    static class PackageInfo implements Comparable<PackageInfo> {
+        int id, revenue, dest, cost;
+
+        PackageInfo(int id, int revenue, int dest, int cost) {
+            this.id = id;
+            this.revenue = revenue;
+            this.dest = dest;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(PackageInfo other) {
+            if (this.cost != other.cost) return Integer.compare(other.cost, this.cost);
+            return Integer.compare(this.id, other.id);
+        }
+    }
+
+    static List<LandInfo>[] lands = new ArrayList[2001];
+    static PriorityQueue<PackageInfo> packages = new PriorityQueue<>();
+
+    // 거리 초기화
+    public static void init() {
+        Arrays.fill(dist, MAX);
+    }
+
+    // 다익스트라 알고리즘
+    public static void dijkstra() {
+        init();
+        dist[start] = 0;
+        PriorityQueue<LandInfo> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.w, b.w));
+        pq.add(new LandInfo(start, 0));
+
+        while (!pq.isEmpty()) {
+            LandInfo now = pq.poll();
+
+            for (LandInfo next : lands[now.u]) {
+                if (dist[next.u] > dist[now.u] + next.w) {
+                    dist[next.u] = dist[now.u] + next.w;
+                    pq.add(new LandInfo(next.u, dist[next.u]));
                 }
             }
         }
     }
-    public static void print(){
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                System.out.printf("%2d ", cost[i][j] == 100000 ? -1 : cost[i][j]  );
-            }
-            System.out.println();
-        }
-    }
-    public static int find(){
-        int ans = -1;
-        int mx = -1;
-        for(int key: products.keySet()){
-            int pid = key;
-            Product p = products.get(key);
-            int r = p.revenue;
-            int d = p.dest;
-            int ct= cost[start][d];
-            if(ct==100000) continue;
-            if(r-ct<0) continue;
 
-            if(mx< r-ct) {
-                mx = r-ct;
-                ans = pid;
-            }
-            if(mx==r-ct){
-                ans = Math.min(ans,pid);
-            }
+    // 양방향 간선 연결
+    public static void func1(Scanner sc) {
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = 0; i < 2001; i++) {
+            lands[i] = new ArrayList<>();
         }
-        if(ans!=-1) {
-            products.remove(ans);
+
+        for (int i = 0; i < m; i++) {
+            int v = sc.nextInt();
+            int u = sc.nextInt();
+            int w = sc.nextInt();
+            lands[v].add(new LandInfo(u, w));
+            lands[u].add(new LandInfo(v, w));
         }
-        return ans;
+        dijkstra();
     }
 
+    // 패키지 생성
+    public static void func2(Scanner sc) {
+        int id = sc.nextInt();
+        int revenue = sc.nextInt();
+        int dest = sc.nextInt();
+        int cost = (dist[dest] == MAX) ? -1 : revenue - dist[dest];
+        packages.add(new PackageInfo(id, revenue, dest, cost));
+        checkId[id] = true;
+    }
+
+    // 패키지 취소
+    public static void func3(Scanner sc) {
+        int id = sc.nextInt();
+        checkId[id] = false;
+    }
+
+    // 패키지 판매
+    public static void func4() {
+        if (packages.isEmpty()) {
+            System.out.println(-1);
+            return;
+        }
+
+        while (!packages.isEmpty()) {
+            PackageInfo now = packages.peek();
+            if (!checkId[now.id]) {
+                packages.poll();
+            } else {
+                break;
+            }
+        }
+
+        if (packages.isEmpty()) {
+            System.out.println(-1);
+        } else {
+            PackageInfo now = packages.poll();
+            if (now.cost < 0) {
+                System.out.println(-1);
+            } else {
+                checkId[now.id] = false;
+                System.out.println(now.id);
+            }
+        }
+    }
+
+    // 출발지 변경
+    public static void func5(Scanner sc) {
+        start = sc.nextInt();
+        dijkstra();
+
+        PriorityQueue<PackageInfo> temp = new PriorityQueue<>();
+        while (!packages.isEmpty()) {
+            PackageInfo now = packages.poll();
+            if (!checkId[now.id]) continue;
+            temp.add(now);
+        }
+
+        while (!temp.isEmpty()) {
+            PackageInfo now = temp.poll();
+            int dest = now.dest;
+            int revenue = now.revenue;
+            int cost = (dist[dest] == MAX) ? -1 : revenue - dist[dest];
+            now.cost = cost;
+            packages.add(now);
+        }
+    }
+
+    // 명령 입력
+    public static void input(Scanner sc) {
+        for (int i = 0; i < Q; i++) {
+            int num = sc.nextInt();
+            switch (num) {
+                case 100:
+                    func1(sc);
+                    break;
+                case 200:
+                    func2(sc);
+                    break;
+                case 300:
+                    func3(sc);
+                    break;
+                case 400:
+                    func4();
+                    break;
+                case 500:
+                    func5(sc);
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Q = sc.nextInt();
+        input(sc);
+    }
 }
